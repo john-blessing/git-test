e0a84d0ff30679fd87875b0a2b45b2a04853cee2
kinhajimefuku <1585185302@qq.com>
2021-07-30 00:57:51 +0800

    commit

--- demo1.js
+++ demo1.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2021-07-30 00:54:49
+ * @LastEditTime: 2021-07-30 00:57:43
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /git-test/demo1.js
+ */
 const map = new Map;
 map.set('name', 'tom')
 const date = new Date()
@@ -7,3 +15,6 @@ console.log(date.getFullYear())
 console.log('hello wrold')
 
 console.log('next')
+
+
+const s = new Set();
